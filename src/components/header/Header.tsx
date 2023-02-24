07e8bbc51245c95632d88ce06ccec5ef229acedb
jan-clod <paviliong6tim@gmail.com>
2023-02-24 15:12:30 +0300

    second varian 0.0.2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,12 @@
 import s from './Header.module.css'
+import 'animate.css';
 import { Nav } from '../nav/Nav'
 
 export const Header = () => {
 
     return (
         <div className={s.head}>
-            <h1>
+            <h1 className={` ${"animate__animated animate__zoomInLeft"}`}>
                 _Jan_clod_
             </h1>
             <Nav />
