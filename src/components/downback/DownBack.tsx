0eb6cbc9ff3a09fe173fc8345138cb529a32a3e3
jan-clod <paviliong6tim@gmail.com>
2023-03-06 15:17:32 +0300

    main.png

--- src/components/downback/DownBack.tsx
+++ src/components/downback/DownBack.tsx
@@ -10,7 +10,7 @@ export const DownBack = () => {
     /*     console.log(window.pageYOffset); */
 
     return (
-        <div className={`${s.blockDown}`}>
+        <div className={`${s.blockDown}`} >
 
             <ul>
 
