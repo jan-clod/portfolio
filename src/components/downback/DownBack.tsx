c3904e438a0cc759de0a1f20e594a65b8d0691d6
jan-clod <paviliong6tim@gmail.com>
2023-03-06 17:39:38 +0300

    footer

--- src/components/downback/DownBack.tsx
+++ src/components/downback/DownBack.tsx
@@ -14,14 +14,12 @@ export const DownBack = () => {
 
             <ul>
 
-                <li><img src={mail} alt='404'></img><span>my mail: fia.yan@mail.ru</span></li>
-                <li><img src={telega} alt='404'></img> my <a href='https://t.me/jan_cl0d'> telegram</a> </li>
-                <li><img src={git} alt='404'></img>my  <a href="https://github.com/jan-clod/">gitHub</a></li>
-                <li>Belarus Pinsk</li>
-                <li>+375 29 364 83 53</li>
-                {/*                 <li>width screen:_{wind}</li>
-                <li >height screen:_{height}</li> */}
+                <li><span><img src={mail} alt='404'></img></span></li>
+                <li><a href='https://t.me/jan_cl0d'> <img src={telega} alt='404'></img></a> </li>
+                <li><a href="https://github.com/jan-clod/"><img src={git} alt='404'></img></a></li>
             </ul>
+            <p>+375 29 364 83 53</p>
+            <p>fia.yan@mail.ru</p>
             <h1 className={`${s.h1Y} + ${"animate__animated animate__backInLeft"}`}>
                 _Jan_clod_
             </h1>
