df620dcf8e5fa9aa2dd3b1ae24e52a7a169851bf
jan-clod <paviliong6tim@gmail.com>
2023-03-07 15:42:29 +0300

    ins img

--- src/value.jsx
+++ src/value.jsx
@@ -8,11 +8,4 @@ export let textGIT =['config', 'init', 'commit', 'clone', 'status', 'add', 'pull
 export let textREDUX = ['combineReducers', 'applyMiddleware', 'thunk', 'dispath', 'createStore', 'connect', 'actionCreator', 'reduxToolkit']
 export let textALL = ['GitHub', 'MUI', 'Axios', 'PWA', 'SPA', 'CLI Tools', 'CORS', 'BEM', 'Rest', 'CRUD', 'KISS', 'Dry', 'SOLID']
 
-export const linkJS = 'https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png'
-export const linkCSS = 'https://img.freepik.com/free-icon/css_318-674222.jpg'
-export const linkHTML = 'https://www.clipartmax.com/png/full/171-1719778_html5-html5-logo-png.png'
-export const linkREACT = 'https://storage.googleapis.com/indie-hackers.appspot.com/product-avatars/react-tutorial/9oI3y6pGdIOlO0cYhTeldDBqpWu1?avatarupdatetimestamp=undefined'
-export const linkTS = 'https://w7.pngwing.com/pngs/915/519/png-transparent-typescript-hd-logo-thumbnail.png'
-export const linkGIT = 'https://assets.stickpng.com/images/5847f981cef1014c0b5e48be.png'
-export const linkREDUX = 'https://assets.stickpng.com/images/5848309bcef1014c0b5e4a9a.png'
-export const linkALL = 'https://www.freepnglogos.com/uploads/logo-internet-png/logo-internet-internet-internet-signal-icon-with-png-and-vector-format-27.png'
+
