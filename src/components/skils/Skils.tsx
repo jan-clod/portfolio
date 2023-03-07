0cd0ea8e04bb83d9aa37e57da32043b8b1bcdfdb
jan-clod <paviliong6tim@gmail.com>
2023-03-07 15:42:31 +0300

    ins img

--- src/components/skils/Skils.tsx
+++ src/components/skils/Skils.tsx
@@ -1,21 +1,29 @@
 import s from './../header/Header.module.css'
+import rectImg from './reactImg.png'
+import jsImg from './jsImg.png'
+import tsImg from './tsImg.png'
+import cssImg from './cssImg.png'
+import reduxImg from './reduxImg.png'
+import gitImg from './gitImg.png'
+import htmlImg from './htmlImg.png'
+import allImg from './allImg.png'
 import { Skill } from './skill/Skill'
-import {  textHTML, textJS, textTS, textREACT, textREDUX, textGIT, textALL, textCSS, linkREDUX, linkJS, linkGIT, linkALL, linkCSS, linkHTML, linkREACT, linkTS } from './../../value.jsx'
+import {  textHTML, textJS, textTS, textREACT, textREDUX, textGIT, textALL, textCSS, } from './../../value.jsx'
 
 export const Skils = () => {
     return (
         <div className={`${s.skillsBlock} `}>
             <h1 className={s.skillTitle}>skills</h1>
             <div className={`${s.skilsContainer} ${s.container}`}>
-                <Skill name={'html '} body={textHTML} img={linkHTML} />
-                <Skill name={'css '} body={textCSS} img={linkCSS} />
-                <Skill name={'js '} body={textJS} img={linkJS} />
-                <Skill name={'ts '} body={textTS} img={linkTS} />
+                <Skill name={'html '} body={textHTML} img={htmlImg} />
+                <Skill name={'css '} body={textCSS} img={cssImg} />
+                <Skill name={'js '} body={textJS} img={jsImg} />
+                <Skill name={'ts '} body={textTS} img={tsImg} />
 
-                <Skill name={'react '} body={textREACT} img={linkREACT} />
-                <Skill name={'git '} body={textGIT} img={linkGIT} />
-                <Skill name={'redux '} body={textREDUX} img={linkREDUX} />
-                <Skill name={'all '} body={textALL} img={linkALL} />
+                <Skill name={'react '} body={textREACT} img={rectImg} />
+                <Skill name={'git '} body={textGIT} img={gitImg} />
+                <Skill name={'redux '} body={textREDUX} img={reduxImg} />
+                <Skill name={'all '} body={textALL} img={allImg} />
             </div>
         </div>
     )
